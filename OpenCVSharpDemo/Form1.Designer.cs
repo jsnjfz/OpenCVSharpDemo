377e7ee4f557328f5347f08618999acc9e6a0ec0
jsnjfz <jsnjfz@gmail.com>
2020-04-26 14:27:08 +0800

    First Commit

---
+++ OpenCVSharpDemo/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace OpenCVSharpDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(827, 515);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(827, 515);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(348, 548);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "检测";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 597);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ OpenCVSharpDemo/Form1.cs
@@ -0,0 +1,118 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OpenCVSharpDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string fileName;
+        Mat mat;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fileName = @Application.StartupPath + @"\test.png";
+            mat = new Mat(fileName);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Image = mat.ToBitmap();
+        }
+
+        public void findTextRegion(Mat dilation)
+        {
+            // 1. 查找轮廓
+            OpenCvSharp.Point[][] contours;
+            HierarchyIndex[] hierarchly;
+            Rect biggestContourRect = new Rect();
+
+            Cv2.FindContours(dilation, out contours, out hierarchly, RetrievalModes.Tree, ContourApproximationModes.ApproxSimple);
+
+            // 2. 筛选那些面积小的
+            int i = 0;
+            foreach (OpenCvSharp.Point[] contour in contours)
+            {
+                double area = Cv2.ContourArea(contour);
+
+                //面积小的都筛选掉
+                if (area < 1000)
+                {
+                    continue;
+                }
+
+                //轮廓近似，作用很小
+                double epsilon = 0.001 * Cv2.ArcLength(contour, true);
+
+                //找到最小的矩形
+                biggestContourRect = Cv2.BoundingRect(contour);
+
+                if (biggestContourRect.Height > (biggestContourRect.Width * 1.2))
+                {
+                    continue;
+                }
+
+                //画线
+                mat.Rectangle(biggestContourRect,new Scalar(0, 255, 0), 2);
+            }
+
+            pictureBox1.Image = mat.ToBitmap();
+            //Cv2.ImShow("img", mat);
+        }
+
+
+        public Mat preprocess(string imgPath)
+        {
+            Mat dilation2 = new Mat();
+
+            //读取灰度图
+            using (Mat src = new Mat(imgPath, ImreadModes.Grayscale))
+            {
+                //1.Sobel算子，x方向求梯度
+                Mat sobel = new Mat();
+                Cv2.Sobel(src, sobel, MatType.CV_8U, 1, 0, 3);
+
+                //2.二值化
+                Mat binary = new Mat();
+                Cv2.Threshold(sobel, binary, 0, 255, ThresholdTypes.Otsu | ThresholdTypes.Binary);
+
+                //3. 膨胀和腐蚀操作的核函数
+                Mat element1 = new Mat();
+                Mat element2 = new Mat();
+                OpenCvSharp.Size size1 = new OpenCvSharp.Size(30, 9);
+                OpenCvSharp.Size size2 = new OpenCvSharp.Size(24, 6);
+
+                element1 = Cv2.GetStructuringElement(MorphShapes.Rect, size1);
+                element2 = Cv2.GetStructuringElement(MorphShapes.Rect, size2);
+
+                //4. 膨胀一次，让轮廓突出
+                Mat dilation = new Mat();
+                Cv2.Dilate(binary, dilation, element2);
+
+                //5. 腐蚀一次，去掉细节，如表格线等。注意这里去掉的是竖直的线
+                Mat erosion = new Mat();
+                Cv2.Erode(dilation, erosion, element1);
+
+                //6. 再次膨胀，让轮廓明显一些
+                Cv2.Dilate(erosion, dilation2, element2, null, 3);
+            }
+            return dilation2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Mat test = preprocess(fileName);
+            findTextRegion(test);
+        }
+    }
+}
